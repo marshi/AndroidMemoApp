85963a5f70fb438a3bc2f39f795bd0c0a9e47e8e
marshi <mashi3648@gmail.como>
2021-02-11 20:32:01 +0900

    toolbar and navigation.

--- feature/home/src/main/java/marshi/memo/app/feature/home/MainActivity.kt
+++ feature/home/src/main/java/marshi/memo/app/feature/home/MainActivity.kt
@@ -2,7 +2,10 @@ package marshi.memo.app.feature.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import dev.marshi.feature.home.R
 import dev.marshi.feature.home.databinding.ActivityMainBinding
@@ -10,15 +13,24 @@ import dev.marshi.feature.home.databinding.ActivityMainBinding
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-  private lateinit var binding : ActivityMainBinding
+  private lateinit var binding: ActivityMainBinding
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
 
-    setSupportActionBar(binding.toolbar).apply {
+    setSupportActionBar(binding.toolbar)
+    supportActionBar?.apply {
       title = "title"
+      setDisplayHomeAsUpEnabled(true)
     }
+  }
+
+  override fun onStart() {
+    super.onStart()
+    val navController = findNavController(R.id.nav_host_fragment)
+    val appBarConfiguration = AppBarConfiguration(navController.graph)
+    binding.toolbar.setupWithNavController(navController, appBarConfiguration)
   }
 }
