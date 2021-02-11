71e67dfad14d6da04f3673dca269318c6119b49c
marshi <mashi3648@gmail.como>
2021-02-11 18:52:29 +0900

    toolbar

--- feature/home/src/main/java/marshi/memo/app/feature/home/MainActivity.kt
+++ feature/home/src/main/java/marshi/memo/app/feature/home/MainActivity.kt
@@ -2,14 +2,23 @@ package marshi.memo.app.feature.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import dagger.hilt.android.AndroidEntryPoint
 import dev.marshi.feature.home.R
+import dev.marshi.feature.home.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+  private lateinit var binding : ActivityMainBinding
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    setSupportActionBar(binding.toolbar).apply {
+      title = "title"
     }
+  }
 }
