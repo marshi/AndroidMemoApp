498caf5dce645682523fe3358bf980e5ae74e327
marshi <mashi3648@gmail.como>
2021-02-20 21:56:12 +0900

    popupwindw.

---
+++ core/src/main/java/dev/marshi/memo/core/util/dp.kt
@@ -0,0 +1,10 @@
+package dev.marshi.memo.core.util
+
+import android.content.res.Resources
+import android.util.TypedValue
+
+fun Int.dpToPx(): Float = TypedValue.applyDimension(
+  TypedValue.COMPLEX_UNIT_DIP,
+  this.toFloat(),
+  Resources.getSystem().displayMetrics
+)
--- feature/home/src/main/java/dev/marshi/memo/feature/home/MemoListItem.kt
+++ feature/home/src/main/java/dev/marshi/memo/feature/home/MemoListItem.kt
@@ -1,22 +1,47 @@
 package dev.marshi.memo.feature.home
 
-import android.view.View
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.*
+import android.widget.PopupWindow
 import com.xwray.groupie.viewbinding.BindableItem
 import dev.marshi.memo.core.domain.model.MemoModel
+import dev.marshi.memo.core.util.dpToPx
 import dev.marshi.memo.feature.home.databinding.MemoListItemBinding
+import dev.marshi.memo.feature.home.databinding.PopupMenuBinding
 
 class MemoListItem(private val model: MemoModel) : BindableItem<MemoListItemBinding>(
   model.id.toLong()
 ) {
-  override fun bind(viewBinding: MemoListItemBinding, position: Int) {
-    viewBinding.memo.text = model.text
-  }
-
   override fun getLayout(): Int {
     return R.layout.memo_list_item
   }
 
   override fun initializeViewBinding(view: View): MemoListItemBinding {
     return MemoListItemBinding.bind(view)
+  }
+
+  override fun bind(viewBinding: MemoListItemBinding, position: Int) {
+    viewBinding.memo.text = model.text
+    val popupMenuLayoutBinding =
+      PopupMenuBinding.inflate(LayoutInflater.from(viewBinding.root.context))
+
+    val popupWindow = PopupWindow(
+      popupMenuLayoutBinding.root,
+      ViewGroup.LayoutParams.WRAP_CONTENT,
+      ViewGroup.LayoutParams.WRAP_CONTENT,
+      true
+    ).apply {
+      setBackgroundDrawable(ColorDrawable(Color.WHITE))
+      elevation = 24.dpToPx()
+    }
+    popupMenuLayoutBinding.button1.setOnClickListener {
+      println("delte")
+      popupWindow.dismiss()
+    }
+
+    viewBinding.more.setOnClickListener {
+      popupWindow.showAsDropDown(it, -8.dpToPx().toInt(), 0, Gravity.END)
+    }
   }
 }
